5e93ebd2561a2066fe4fc0727534736416aba4a4
iplnkr <iphoplunker@gmail.com>
2024-11-17 02:02:37 +0000

    Improved Game End Systems

--- GameOff2024/Assets/Scripts/LevelEndTrigger.cs
+++ GameOff2024/Assets/Scripts/LevelEndTrigger.cs
@@ -5,28 +5,57 @@ using UnityEngine;
 
 public class LevelEndTrigger : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera personalVCam;
+    [SerializeField] private CinemachineVirtualCamera personalVCamOutside;
+    [SerializeField] private CinemachineVirtualCamera personalVCamInside;
     [SerializeField] private Animator elevatorAnim;
+    [SerializeField] private Transform outdoorLocation;
+    [SerializeField] private GameObject invisWall;
+    private PlayerController playerController;
+    public bool isStartgameElevator = false;//is this elevator for the start or end of a level
+
+    //ToDo ensure camera panless functionality
 
     void Start()
     {
-        //start by opening door
-        elevatorAnim.SetTrigger("ToggleElevatorState");
+        if(isStartgameElevator)//if elevator at level start
+        {
+            //cut camera to inside
+            if(personalVCamInside != null)
+            {
+                personalVCamInside.enabled = true;
+                personalVCamOutside.enabled = true;
+                //set player position inside lift
+                playerController = FindObjectOfType<PlayerController>();
+                playerController.transform.position = new Vector3(transform.position.x, playerController.transform.position.y, transform.position.z);
+                playerController.transform.eulerAngles = new Vector3(0, Mathf.Round(playerController.transform.rotation.y / 90f) * 90, 0);
+                //disable trigger outside
+                GetComponent<BoxCollider>().enabled = false;
+                //disable player controls
+                playerController.ToggleInputOn(false);//disable input
+                FindObjectOfType<CinemachineInputProvider>().enabled = false;//disable camera controls
+            }
+        }
+        else//level ending elevator
+        {
+            //start by opening door
+            elevatorAnim.SetTrigger("ToggleElevatorState");
+        }
     }
 
     public void OnTriggerEnter(Collider col)
     {
         if(col.tag == "Player")
         {
-            if(PatrolNavigation.gameOverEnemy == null)//stop clash of win and lose condition (only win if not losing)
+            if(PatrolNavigation.gameOverEnemy == null)//stop clash of win and lose condition (only try to win if not losing)
             {
-                PatrolNavigation.gameOverEnemy = new PatrolNavigation();//set value to stop losses happening afterwards
-                col.GetComponent<PlayerController>().ToggleInputOn(false);//disable input
+                PatrolNavigation.gameOverEnemy = new PatrolNavigation();//set static value to stop game losses happening afterwards
+                playerController = col.GetComponent<PlayerController>();
+                playerController.ToggleInputOn(false);//disable input
                 FindObjectOfType<CinemachineInputProvider>().enabled = false;//disable camera controls
                 //pan camera to player
-                if(personalVCam != null)
+                if(personalVCamOutside != null)
                 {
-                    personalVCam.enabled = true;
+                    personalVCamOutside.enabled = true;
                 }
                 else
                 {
@@ -36,12 +65,105 @@ public class LevelEndTrigger : MonoBehaviour
         }
     }
 
-    public void EndGamePostCameraPan()//the engame functionality after the camera pan
+    //Game End Functions--------------------------------------------------------------------------------------------------------------------
+
+    public void EndGamePostCameraPan()//the endgame functionality after the camera pan
     {
-        //ToDo Player walk in
-        //ToDo Animate door close
+        if(!isStartgameElevator)
+        {
+            //Player walk in
+            playerController.isAutomoving = true;
+            playerController.autoDestinationQueue.Add(outdoorLocation.transform.position);
+            playerController.autoDestinationQueue.Add((transform.position - outdoorLocation.transform.position).normalized * 0.15f + transform.position);
+            playerController.autoDestinationQueue.Add(transform.position);
+            StartCoroutine(EndGamePostCarAnimation());
+        }
+    }
+
+    IEnumerator EndGamePostCarAnimation()
+    {
+        //wait until car inside
+        float dist = Vector3.Distance(new Vector3(playerController.transform.position.x, 0, playerController.transform.position.z), new Vector3(transform.position.x, 0, transform.position.z));
+        while(dist > 1)
+        {
+            yield return null;
+            dist = Vector3.Distance(new Vector3(playerController.transform.position.x, 0, playerController.transform.position.z), new Vector3(transform.position.x, 0, transform.position.z));
+        }
+        //Animate door close
+        yield return new WaitForSeconds(0.5f);
         elevatorAnim.SetTrigger("ToggleElevatorState");
+        yield return new WaitForSeconds(1f);
+        //fix position and rotation
+        playerController.transform.position = new Vector3(transform.position.x, playerController.transform.position.y, transform.position.z);
+        playerController.transform.eulerAngles = new Vector3(0, Mathf.Round(playerController.transform.eulerAngles.y / 90f) * 90, 0);
+        yield return new WaitForSeconds(1f);
+        //cut camera to inside
+        if(personalVCamInside != null)
+        {
+            personalVCamInside.enabled = true;
+        }
+        playerController.transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
         //ToDo Win Game Here
         Debug.Log("ToDo Won Game");
+    }
+
+    //Game Start Functions--------------------------------------------------------------------------------------------------------------------
+
+    public void StartGameCameraPan()//the endgame functionality after the camera pan
+    {
+        if(isStartgameElevator)
+        {
+            playerController.transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            StartCoroutine(StartGamePostCarAnimation());
+        }
+    }
+
+    IEnumerator StartGamePostCarAnimation()
+    {
+        yield return new WaitForSeconds(0.5f);
+        //Player walk out after doors open
+        playerController.isAutomoving = true;
+        elevatorAnim.SetTrigger("ToggleElevatorState");
+        playerController.autoDestinationQueue.Add((outdoorLocation.transform.position - transform.position).normalized * 10.0f + outdoorLocation.transform.position);
+        //move camera
+        if(personalVCamOutside != null)
+        {
+            personalVCamInside.enabled = false;
+        }
+        //wait until car outside
+        float dist = Vector3.Distance(new Vector3(playerController.transform.position.x, 0, playerController.transform.position.z), new Vector3(outdoorLocation.transform.position.x, 0, outdoorLocation.transform.position.z));
+        while(dist > 1)
+        {
+            yield return null;
+            dist = Vector3.Distance(new Vector3(playerController.transform.position.x, 0, playerController.transform.position.z), new Vector3(outdoorLocation.transform.position.x, 0, outdoorLocation.transform.position.z));
+        }
+        //close doors
+        elevatorAnim.SetTrigger("ToggleElevatorState");
+        //wait until at destination
+        Vector3 destination = (outdoorLocation.transform.position - transform.position).normalized * 10.0f + outdoorLocation.transform.position;
+        dist = Vector3.Distance(new Vector3(playerController.transform.position.x, 0, playerController.transform.position.z), new Vector3(destination.x, 0, destination.z));
+        while(dist > 1)
+        {
+            yield return null;
+            dist = Vector3.Distance(new Vector3(playerController.transform.position.x, 0, playerController.transform.position.z), new Vector3(destination.x, 0, destination.z));
+        }
+        playerController.CancelAutoMovement();
+        //move to gameplay camera
+        if(personalVCamOutside != null)
+        {
+            playerController.SetCameraYAngleManual(transform.eulerAngles.y);
+            personalVCamOutside.enabled = false;
+        }
+        //fix position and rotation
+        playerController.transform.position = destination;
+        playerController.transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        yield return new WaitForSeconds(0.5f);
+        //create collision on doors
+        invisWall.GetComponent<BoxCollider>().enabled = true;
+        //re-enable movement
+        playerController = FindObjectOfType<PlayerController>();
+        playerController.ToggleInputOn(true);//disable input
+        FindObjectOfType<CinemachineInputProvider>().enabled = true;//disable camera controls
+        
     }
 }
--- GameOff2024/Assets/Scripts/Player/PlayerController.cs
+++ GameOff2024/Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.Scripting;
 
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private CinemachineFreeLook mainVCam;
     [SerializeField] private Camera playerCamera;
     [SerializeField] private Animator playerAnim;
 
@@ -18,6 +20,7 @@ public class PlayerController : MonoBehaviour
     public float runSpeed;
     [SerializeField] float sprintMult;
 
+    [Header("Vertical Movement")]
     private float gravity = -9.81f;
     [SerializeField] float gravMult = 3;
     private float verticalVelocity = 0;
@@ -33,6 +36,10 @@ public class PlayerController : MonoBehaviour
     public int disguisesOwned = 1;
     public bool isTryingToHide = true;
 
+    [Header("Auto-Movement")]
+    public bool isAutomoving = false;
+    public List<Vector3> autoDestinationQueue = new List<Vector3>();
+    private float autoDestTimeout = 0;//time limit for auto-movement
 
     [Header("Camera")]
     public float turnSmoothTime = 0.1f;
@@ -53,15 +60,25 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        movementToApply = new Vector3(0, 0, 0);
-        HandleGravity();
-        HandleJump();
-        HandleCameraAndMovement();
-        HandleFlash();
+        movementToApply = new Vector3(0, 0, 0);  
+        if(!isAutomoving)//if player is controlling movement
+        {
+            HandleGravity();
+            HandleJump();
+            HandleCameraAndMovement();
+            HandleFlash();
 
-        //set animation parameters
-        playerAnim.SetBool("isWalking", (playerLocomotionInput.MovementInput.magnitude > 0.1f));
-        playerAnim.SetBool("isSprinting", (playerLocomotionInput.SprintInput > 0.1f));
+            //set animation parameters
+            playerAnim.SetBool("isWalking", (playerLocomotionInput.MovementInput.magnitude > 0.1f));
+            playerAnim.SetBool("isSprinting", (playerLocomotionInput.SprintInput > 0.1f));
+        }
+        else//if automatic movement
+        {        
+            HandleGravity();
+            HandleJump();
+            HandleAutoMovement();
+            playerAnim.SetBool("isWalking", (autoDestinationQueue.Count >= 1));
+        }
         if(verticalVelocity < -10)//if falling
         {
             if(!playerAnim.GetBool("isFalling"))
@@ -75,15 +92,29 @@ public class PlayerController : MonoBehaviour
     //calculate camera after movement
     private void LateUpdate()
     {
-        //when player moving
-        if(playerLocomotionInput.MovementInput.magnitude > 0.1f)
+        if(!isAutomoving)//if player is controlling movement
         {
-            Vector3 inpDirection = new Vector3(playerLocomotionInput.MovementInput.x, 0, playerLocomotionInput.MovementInput.y).normalized;
+            //when player moving
+            if(playerLocomotionInput.MovementInput.magnitude > 0.1f)
+            {
+                Vector3 inpDirection = new Vector3(playerLocomotionInput.MovementInput.x, 0, playerLocomotionInput.MovementInput.y).normalized;
 
-            //set the rotation to facing direction
-            float targAng = Mathf.Atan2(inpDirection.x, inpDirection.z) * Mathf.Rad2Deg + playerCamera.transform.eulerAngles.y;
-            float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targAng, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, smoothedAngle, 0f);
+                //set the rotation to facing direction
+                float targAng = Mathf.Atan2(inpDirection.x, inpDirection.z) * Mathf.Rad2Deg + playerCamera.transform.eulerAngles.y;
+                float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targAng, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, smoothedAngle, 0f);
+            }
+        }
+        else
+        {
+            if(autoDestinationQueue.Count >= 1)
+            {
+                Vector3 inpDirection = (new Vector3((autoDestinationQueue[0].x - transform.position.x), 0, (autoDestinationQueue[0].z - transform.position.z))).normalized;
+
+                //set the rotation to facing direction
+                float targAng = Mathf.Atan2(inpDirection.x, inpDirection.z) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0f, targAng, 0f);
+            }
         }
     }
 
@@ -172,5 +203,66 @@ public class PlayerController : MonoBehaviour
         {
             playerLocomotionInput.CarControls.Disable();
         }
+    }
+
+    //Auto movement -----------------------------------------------------------------------------------------------------------
+    private void HandleAutoMovement()
+    {
+        if(autoDestinationQueue.Count >= 1)//if there is a destination
+        {
+            
+            Vector3 moveDirection = (new Vector3((autoDestinationQueue[0].x - transform.position.x), 0, (autoDestinationQueue[0].z - transform.position.z))).normalized;
+            moveDirection = (autoDestinationQueue[0] - transform.position);
+            moveDirection = new Vector3(moveDirection.x, 0 ,moveDirection.z).normalized;
+
+            Vector3 moveDelta = moveDirection * runAcceleration * Time.deltaTime;
+            Vector3 newVel = characterController.velocity + moveDelta;
+            newVel.y = 0;//no vertical speed limit
+
+            //apply drag
+            Vector3 curDrag = newVel.normalized * drag * Time.deltaTime;
+            newVel = (newVel.magnitude > drag * Time.deltaTime) ? newVel - curDrag : Vector3.zero;
+
+            //apply movement
+            movementToApply += newVel;
+            movementToApply.y += verticalVelocity;//and gravity and jump
+            //move player
+            characterController.Move(movementToApply * Time.deltaTime);
+
+            autoDestTimeout += Time.deltaTime;
+            if(autoDestTimeout > 3)//set 3 second limit to avoid impossible movement
+            {
+                autoDestinationQueue.RemoveAt(0);
+                autoDestTimeout = 0;
+                if(autoDestinationQueue.Count == 0)
+                {
+                    isAutomoving = false;
+                }
+            }
+            else
+            {
+                float dist = Vector3.Distance(new Vector3(autoDestinationQueue[0].x, 0, autoDestinationQueue[0].z), new Vector3(transform.position.x, 0, transform.position.z));
+                if(dist < 0.05f)//if close enough to destination
+                {
+                    autoDestinationQueue.RemoveAt(0);
+                    autoDestTimeout = 0;
+                    if(autoDestinationQueue.Count == 0)
+                    {
+                        isAutomoving = false;
+                    }
+                }
+            }
+        }
+    }
+
+    public void CancelAutoMovement()
+    {
+        autoDestinationQueue.Clear();
+        isAutomoving = false;
+    }
+
+    public void SetCameraYAngleManual(float ang)
+    {
+        mainVCam.m_XAxis.Value = ang;
     }
 }
